1cf18fb9ef3669e82414a113bd317086bdbbf118
Gustav Jonsson <gustav.jonsson@gmail.com>
2015-06-19 16:38:38 +0200

    Installed passport and passport-local. Created usermodel file.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/ping",function(req,res) {
+	res.send("pong",200);
+})
+
 module.exports = router;
